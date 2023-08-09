7792343dccb198f934170591e093695b9437e4c2
Brums16 <chrisbrummitt16@hotmail.co.uk>
2023-08-09 01:27:36 +0100

    adding the app to github

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import './Style.css'
+
+
+export default function Header () {
+    return (
+        <div className="header">
+            <h2>Dashboard</h2>
+            <h2>Pick a Card</h2>
+            <h2 className="settings">Settings</h2>
+        </div>
+        
+    )
+}
